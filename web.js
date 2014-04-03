7d8f58e63a30c714a5ded0bfcfecfecd042c99ed
reachjalil <reachjalil@gmail.com>
2014-04-03 18:30:01 +0100

    allow cross browser first test

--- web.js
+++ web.js
@@ -1,9 +1,28 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
 app.use(logfmt.requestLogger());
-
+app.configure(function () {
+  app.use(allowCrossDomain);
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 app.get('/', function(req, res) {
   res.send('Hello World! Whats Up');
 });
